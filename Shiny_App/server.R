88a4cb8950790254f17d96e52ad2354701410ac9
Songui <54897807+Songui@users.noreply.github.com>
2019-10-01 11:05:07 +0200

    Partie DATA
    DATA_PART

--- Shiny_App/server.R
+++ Shiny_App/server.R
@@ -3,14 +3,25 @@
 
 #On doit permettre à l'utilisateur de faire entrer la variable target y
 #######The minority class must be coded 1 and the majority 0 for the unbalanced family function ??? #####
-####### We should imposed that the class have to be the last one of the data column and called "Class" ?? #####
+####### We should imposed that the class have to be the last one of the data column and called "class" ?? #####
 
+library(FNN)
 library(shiny)
+library(summarytools)
 library(DescTools)
 library(smotefamily)
 library(unbalanced)
 library(DataExplorer)
-library(summarytools)
+library(DT)
+library(dplyr)
+library(corrplot)
+library(haven)
+library(readxl)
+library(shinydashboard)
+library(DescTools)
+library(plotly)
+library(shinydashboardPlus)
+
 
 oversampling = function (data, p, position) 
 {
@@ -61,45 +72,121 @@ options(shiny.maxRequestSize=150*1024^2)
 
 shinyServer(function(input, output) {
  
-  data = reactive ({
-    file = input$loading
-    if (is.null(file)){return ()}
-    data = read.csv (file$datapath)
-    data$Class = as.factor(data$Class)
-    data
-    }) #Chargement de la table
-
-
-  ###################### DATA PART ##############################
+ 
+###################### DATA PART ##############################  
   
-  output$plot1 = renderPlot({
-    if (is.null(data())) {return()}
-    x= plot_str(data())
-    x
+  output$file_options = renderUI({
+    
+    switch(input$type_file,
+           "CSV" = list(radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ','),
+                        checkboxInput(inputId = 'header', label =strong('Header'), value = TRUE),
+                        radioButtons(inputId = 'dec', label = 'Decimal', choices = c(Point='.', Comma=','), selected = '.')
+             ),
+           "TXT" = list(radioButtons(inputId = 'sep', label = 'Separator', choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''), selected = ','),
+                   checkboxInput(inputId = 'header', label = strong('Header'), value = TRUE),
+                   radioButtons(inputId = 'dec', label = 'Decimal', choices = c(Point=".", Comma=','), selected = '.')
+             ),
+           "EXCEL" = checkboxInput(inputId = 'header', label = strong('Header'), value = TRUE))
     
   })
   
   
+  raw_data = reactive ({
+    validate(
+      need(is.null(input$loading)== FALSE, "Please select a file")
+    )
+    
+    file = input$loading
+ 
+    data= switch(input$type_file,
+           "CSV" = read.csv (file$datapath, header = input$header, sep = input$sep, dec = input$dec), 
+           "TXT" = read.table (file$datapath, header = input$header, sep = input$sep, dec = input$dec),
+           "EXCEL" = read_excel(file$datapath, col_names = input$header),
+           "SAS" = read_sas(file$datapath)
+           )
+    as.data.frame(data)
+    
+    }) 
+
+  
+  output$target_ui = renderUI({
+  
+    selectInput("targetvar", "Select your target variable",names(raw_data()))
+    
+  })
   
   
+  data = reactive ({
+    validate(
+      need(is.null(input$loading) == FALSE, "Please select a file")
+    )  
+    data = raw_data() 
+    data[,input$targetvar] = as.factor(data[,input$targetvar])
+    names(data)[which(names(data) == input$targetvar)] = "class"
+    data
+  })
   
   
+  target_position = reactive({
+    validate(
+      need(is.null(data()) == FALSE, "Please select a file")
+    )
+    which(names(data()) == "class")
+    
+    })
+  
+  output$other_var_ui = renderUI({
+    validate(
+      need(is.null(input$targetvar) == FALSE, "Please select a file")
+    )
+    
+    selectInput("other_var", "Select variables to withdraw from modelisation",names(data())[- target_position()],multiple = TRUE)
+    })
   
   
+  output$data = DT::renderDataTable(data () , options = list(scrollX = TRUE))
+  
+   
+  output$plot3 = renderPlot({
+    
+    data = as.matrix(dplyr::select_if(data(), is.numeric))
+    
+    corrplot(cor(data,use = "complete.obs"),tl.col="black")
+  })
   
   
+  output$plot2 = renderPlot(
+  
+    ggplot(data=data()) + geom_bar(mapping = aes(x=class))
+  )
   
   
+  output$plot1 = renderUI(
+
+    print(dfSummary(data(), graph.magnif = 0.8), 
+              method = 'render',
+              headings = FALSE,
+              bootstrap.css = FALSE)
+  )
   
   
+ output$sum1 = renderPrint( introduce(data()) )
+  
+ 
+ output$boxplot_var = renderUI(
+   
+     selectInput("boxplot","Choose one variable for BoxPlot",names(dplyr::select_if(data(), is.numeric   )))
+)
+ 
+ output$plot4 = renderPlot(
+   
+   ggplot(data = data()) + 
+     geom_boxplot(mapping = aes(x = class, y = get(input$boxplot))) +
+       labs(y = input$boxplot)
+ )
   
   
-  
-  
-  
-  
-  
-  ###################### SAMPLING PART ##############################
+    ###################### SAMPLING PART ##############################
   
   train = reactive({
     data_nrow = nrow(data())
@@ -112,7 +199,7 @@ shinyServer(function(input, output) {
   test_sample = reactive(data()[-train(),]) #Echantillon test
   
   r_event_freq = reactive({
-    freq = Freq(train_sample()$Class)
+    freq = Freq(train_sample()$class)
     min(freq$perc)
     }) #la fréquence de l'évènement rare
   
@@ -131,16 +218,15 @@ shinyServer(function(input, output) {
   
   
   resampling_train = reactive({
-    if(is.null(data())) {return()}
     
     train_sample = train_sample()
-    y = train_sample$Class
+    y = train_sample$class
     proba = input$p
     r_event_freq = r_event_freq()
     p1 = try({if (proba==1) {0.999} else {max(proba,r_event_freq)}}, silent=T)
     p2 = try({max(proba,r_event_freq)}, silent=T)
     
-    y_position = which(names(train_sample)=="Class")
+    y_position = which(names(train_sample)=="class")
     dup_size = try(floor(input$dup_size),silent = T)
     k_near = try(floor(input$k_near),silent = T)
     
@@ -150,16 +236,16 @@ shinyServer(function(input, output) {
            "SMOTE" = SMOTE(train_sample[,-y_position],y, dup_size=dup_size, K= k_near)$data ,
            "ADASYN" = ADAS(train_sample[,-y_position],y, K=k_near)$data,
            "Tomek-Link" = {data = ubTomek(X=train_sample[,-y_position], Y= train_sample[,y_position]);
-                             cbind(data$X,Class=data$Y)},
+                             cbind(data$X,class=data$Y)},
            "Tomek-Link + Undersampling" = {data = ubTomek(X=train_sample[,-y_position], Y= train_sample[,y_position]);
-                                           newData = cbind(data$X,Class=data$Y);
+                                           newData = cbind(data$X,class=data$Y);
                                            undersampling(newData,p2 ,y_position)},
            "Condensed Nearest Neighbor" = {data = ubCNN(X=train_sample[,-y_position], Y= train_sample[,y_position]);
-                                                  cbind(data$X,Class=data$Y) }
+                                                  cbind(data$X,class=data$Y) }
     )
   })
   
-  output$out = renderDataTable(tail(resampling_train())) #TEST pour montrer que ça marche, à delete plus tard
+  output$out = renderDataTable(resampling_train ()) #TEST pour montrer que ça marche, à delete plus tard
     
   
 
--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -1,22 +1,36 @@
 #Projet_SVM_AKS
+
 library(shiny)
+library(summarytools)
+library(DescTools)
+library(smotefamily)
+library(unbalanced)
+library(DataExplorer)
+library(DT)
+library(dplyr)
+library(corrplot)
+library(haven)
+library(readxl)
 library(shinydashboard)
 library(DescTools)
+library(plotly)
+library(shinydashboardPlus)
+
 
 shinyUI(
   
-  dashboardPage(skin="red", 
-               
-    dashboardHeader(
+  dashboardPagePlus(
+    header = dashboardHeaderPlus(
       title = "FRAUD DETECTION",
       dropdownMenu(type = "notifications", notificationItem(
         text = "Download notice downside",
         icon("bell"),
         status = "info"
-      ))
+      )),
+      enable_rightsidebar = TRUE,
+      rightSidebarIcon = "gears"
     ),
-    
-    dashboardSidebar(
+    sidebar = dashboardSidebar(
       sidebarMenu(
         menuItem("Home", tabName = "home",icon =icon("home")),
         menuItem("Data", tabName = "data",icon =icon("database")),
@@ -25,85 +39,119 @@ shinyUI(
                  menuSubItem("SVM method", tabName = "svm"),
                  menuSubItem("Benchmarking analysis", tabName = "benchmark")),
         menuItem("Prediction", tabName = "prediction", icon = icon("dashboard"))
-    )),
-    
-    dashboardBody(
+      )),
+    body = dashboardBody(
       tabItems(
         tabItem(tabName = "home",
-                box(h3("Welcome to the credit card fraud detection app !", align="center"), width=12, background = "navy"),
+                fluidPage(
+                  box(h3("Welcome to the credit card fraud detection app !", align="center"), width=12, background = "navy"),
+                  
+                  box(h5(p("This app allows you to detect a case of credit card fraud, quickly and easily by mainly using", em(strong("Supports Vectors Machines.")), "By default, we use data from Machine Learning Group (MLG) of Brussels University.")), width=12),
+                  
+                  box(h3(strong("Context")),
+                      p("It is important that credit card companies are able to recognize fraudulent credit card transactions so that customers are not charged for items that they did not purchase."),
+                      h3(strong("Content")),
+                      p("The datasets contains transactions made by credit cards in September 2013 by european cardholders. This dataset presents transactions that occurred in two days, where we have 492 frauds out of 284,807 transactions. The dataset is highly unbalanced, the positive class (frauds) account for 0.172% of all transactions."),
+                      p("It contains only numerical input variables which are the result of a PCA transformation. Unfortunately, due to confidentiality issues, we cannot provide the original features and more background information about the data. Features V1, V2, ... V28 are the principal components obtained with PCA, the only features which have not been transformed with PCA are 'Time' and 'Amount'. Feature 'Time' contains the seconds elapsed between each transaction and the first transaction in the dataset. The feature 'Amount' is the transaction Amount, this feature can be used for example-dependant cost-senstive learning. Feature 'Class' is the response variable and it takes value 1 in case of fraud and 0 otherwise."),
+                      h3(strong("Inspiration")),
+                      p("Identify fraudulent credit card transactions."),
+                      p("Given the class imbalance ratio, we recommend measuring the accuracy using the Area Under the Precision-Recall Curve (AUPRC). Confusion matrix accuracy is not meaningful for unbalanced classification."),
+                      h3(strong("Acknowledgements")),
+                      p("The dataset has been collected and analysed during a research collaboration of Worldline and the Machine Learning Group (http://mlg.ulb.ac.be) of ULB (Université Libre de Bruxelles) on big data mining and fraud detection. More details on current and past projects on related topics are available on https://www.researchgate.net/project/Fraud-detection-5 and the page of the DefeatFraud project"),
+                      p("Please cite the following works:"),
+                      p("Andrea Dal Pozzolo, Olivier Caelen, Reid A. Johnson and Gianluca Bontempi. Calibrating Probability with Undersampling for Unbalanced Classification. In Symposium on Computational Intelligence and Data Mining (CIDM), IEEE, 2015"),
+                      p("Dal Pozzolo, Andrea; Caelen, Olivier; Le Borgne, Yann-Ael; Waterschoot, Serge; Bontempi, Gianluca. Learned lessons in credit card fraud detection from a practitioner perspective, Expert systems with applications,41,10,4915-4928,2014, Pergamon"),
+                      p("Dal Pozzolo, Andrea; Boracchi, Giacomo; Caelen, Olivier; Alippi, Cesare; Bontempi, Gianluca. Credit card fraud detection: a realistic modeling and a novel learning strategy, IEEE transactions on neural networks and learning systems,29,8,3784-3797,2018,IEEE"),
+                      p("Dal Pozzolo, Andrea Adaptive Machine learning for credit card fraud detection ULB MLG PhD thesis (supervised by G. Bontempi"),
+                      p("Carcillo, Fabrizio; Dal Pozzolo, Andrea; Le Borgne, Yann-Aël; Caelen, Olivier; Mazzer, Yannis; Bontempi, Gianluca. Scarff: a scalable framework for streaming credit card fraud detection with Spark, Information fusion,41, 182-194,2018,Elsevier"),
+                      p("Carcillo, Fabrizio; Le Borgne, Yann-Aël; Caelen, Olivier; Bontempi, Gianluca. Streaming active learning strategies for real-life credit card fraud detection: assessment and visualization, International Journal of Data Science and Analytics, 5,4,285-300,2018,Springer International Publishing"),
+                      p("Bertrand Lebichot, Yann-Aël Le Borgne, Liyun He, Frederic Oblé, Gianluca Bontempi Deep-Learning Domain Adaptation Techniques for Credit Cards Fraud Detection, INNSBDDL 2019: Recent Advances in Big Data and Deep Learning, pp 78-88, 2019"),
+                      p("Fabrizio Carcillo, Yann-Aël Le Borgne, Olivier Caelen, Frederic Oblé, Gianluca Bontempi Combining Unsupervised and Supervised Learning in Credit Card Fraud Detection Information Sciences, 2019"),
+                      
+                      title="Data description from MLG kaggle page",status = "primary", solidHeader = TRUE,collapsible = TRUE, width = 12))
                 
-                box(h5(p("This app allows you to detect a case of credit card fraud, quickly and easily by mainly using", em(strong("Supports Vectors Machines.")), "By default, we use data from Machine Learning Group (MLG) of Brussels University.")), width=12),
-                
-                box(h3(strong("Context")),
-                    p("It is important that credit card companies are able to recognize fraudulent credit card transactions so that customers are not charged for items that they did not purchase."),
-                    h3(strong("Content")),
-                    p("The datasets contains transactions made by credit cards in September 2013 by european cardholders. This dataset presents transactions that occurred in two days, where we have 492 frauds out of 284,807 transactions. The dataset is highly unbalanced, the positive class (frauds) account for 0.172% of all transactions."),
-                    p("It contains only numerical input variables which are the result of a PCA transformation. Unfortunately, due to confidentiality issues, we cannot provide the original features and more background information about the data. Features V1, V2, ... V28 are the principal components obtained with PCA, the only features which have not been transformed with PCA are 'Time' and 'Amount'. Feature 'Time' contains the seconds elapsed between each transaction and the first transaction in the dataset. The feature 'Amount' is the transaction Amount, this feature can be used for example-dependant cost-senstive learning. Feature 'Class' is the response variable and it takes value 1 in case of fraud and 0 otherwise."),
-                    h3(strong("Inspiration")),
-                    p("Identify fraudulent credit card transactions."),
-                    p("Given the class imbalance ratio, we recommend measuring the accuracy using the Area Under the Precision-Recall Curve (AUPRC). Confusion matrix accuracy is not meaningful for unbalanced classification."),
-                    h3(strong("Acknowledgements")),
-                    p("The dataset has been collected and analysed during a research collaboration of Worldline and the Machine Learning Group (http://mlg.ulb.ac.be) of ULB (Université Libre de Bruxelles) on big data mining and fraud detection. More details on current and past projects on related topics are available on https://www.researchgate.net/project/Fraud-detection-5 and the page of the DefeatFraud project"),
-                    p("Please cite the following works:"),
-                    p("Andrea Dal Pozzolo, Olivier Caelen, Reid A. Johnson and Gianluca Bontempi. Calibrating Probability with Undersampling for Unbalanced Classification. In Symposium on Computational Intelligence and Data Mining (CIDM), IEEE, 2015"),
-                    p("Dal Pozzolo, Andrea; Caelen, Olivier; Le Borgne, Yann-Ael; Waterschoot, Serge; Bontempi, Gianluca. Learned lessons in credit card fraud detection from a practitioner perspective, Expert systems with applications,41,10,4915-4928,2014, Pergamon"),
-                    p("Dal Pozzolo, Andrea; Boracchi, Giacomo; Caelen, Olivier; Alippi, Cesare; Bontempi, Gianluca. Credit card fraud detection: a realistic modeling and a novel learning strategy, IEEE transactions on neural networks and learning systems,29,8,3784-3797,2018,IEEE"),
-                    p("Dal Pozzolo, Andrea Adaptive Machine learning for credit card fraud detection ULB MLG PhD thesis (supervised by G. Bontempi"),
-                    p("Carcillo, Fabrizio; Dal Pozzolo, Andrea; Le Borgne, Yann-Aël; Caelen, Olivier; Mazzer, Yannis; Bontempi, Gianluca. Scarff: a scalable framework for streaming credit card fraud detection with Spark, Information fusion,41, 182-194,2018,Elsevier"),
-                    p("Carcillo, Fabrizio; Le Borgne, Yann-Aël; Caelen, Olivier; Bontempi, Gianluca. Streaming active learning strategies for real-life credit card fraud detection: assessment and visualization, International Journal of Data Science and Analytics, 5,4,285-300,2018,Springer International Publishing"),
-                    p("Bertrand Lebichot, Yann-Aël Le Borgne, Liyun He, Frederic Oblé, Gianluca Bontempi Deep-Learning Domain Adaptation Techniques for Credit Cards Fraud Detection, INNSBDDL 2019: Recent Advances in Big Data and Deep Learning, pp 78-88, 2019"),
-                    p("Fabrizio Carcillo, Yann-Aël Le Borgne, Olivier Caelen, Frederic Oblé, Gianluca Bontempi Combining Unsupervised and Supervised Learning in Credit Card Fraud Detection Information Sciences, 2019"),
-                    
-                    title="Data description from MLG kaggle page",status = "primary", solidHeader = TRUE,collapsible = TRUE, width = 12)
-          
         ),
         tabItem(tabName = "data",
-                
-                box(fileInput("loading",""),background = "navy", title="Load data",width = 3, solidHeader = TRUE, status = "primary" ),
-                
-                
-                tabBox (width = 9,
-                    title="Statistics and graphics", id = "stat",
-                    tabPanel("Summary",
-                             plotOutput("plot1")),
-                    tabPanel("Visualisation","MISE EN PLACE DE GRAPHIQUES DYNAMIQUES OU STATIQUES AVEC PLOTLY ET GGPLOT")
+                fluidPage(
+                  
+                  box(selectInput("type_file", "Type of file", c("CSV","TXT","EXCEL","SAS")), 
+                      fileInput("loading",""),
+                      uiOutput("file_options"),
+                      background = "navy", title="Load data",width = 4, solidHeader = TRUE, status = "primary",collapsible = TRUE ),
+                  
+                  box(uiOutput("target_ui"),
+                      background = "navy",title="Target Variable",width= 4, solidHeader = TRUE, status = "primary",collapsible = TRUE, footer = helpText("Make sure that you choose categorical variable in order to avoid errors !")),
+                  
+                  box(uiOutput("other_var_ui"),
+                      background = "navy",width= 4,title="Others Variables", solidHeader = TRUE, status = "primary",collapsible = TRUE, footer = helpText("You can't remove all variables !",br(),br())),
+                  
+                  tabBox (width = 12,title="Statistics and graphics", id = "stat",
+                          tabPanel("Data",
+                                   DT::dataTableOutput("data")
+                          ),
+                          tabPanel("Summary",
+                                   verbatimTextOutput("sum1"),
+                                   htmlOutput("plot1")
+                          ),
+                          tabPanel("Graphics",
+                                   fluidRow(column(width=6,
+                                                   box(plotOutput("plot2"),
+                                                       background = "navy", width = 12,title="Target Variable Barplot", solidHeader = TRUE,status = "primary",collapsible = TRUE)),
+                                            column(width=6,
+                                                   box(plotOutput("plot3"),
+                                                       background = "navy",width = 12, title="Correlation Matrix",solidHeader = TRUE ,status = "primary",collapsible = TRUE))
+                                   ),
+                                   fluidRow(column(width=2),
+                                            column(width=10,
+                                                   box(column(width=3 ,uiOutput("boxplot_var")),
+                                                       column(width=9 , plotOutput("plot4")),
+                                                       background = "navy", width = 10,title="BoxPlot",solidHeader = TRUE,status = "primary",collapsible = TRUE)),
+                                            column(width=2)
+                                            
+                                   )
+                          )
                   )
-                
-                
+                )      
         ),
         tabItem(tabName = "svm" , 
-                
-                tabBox (width=12,
-                        title="SVM", id = "svm",
-                        tabPanel("Description","ICI NOUS ALLONS DECRIRE LA METHODE SVM"),
-                        tabPanel("Implementation","IMPLEMENTATION DU SVM")
-                )
-                
-        ),
+                fluidPage(
+                  tabBox (width=12,
+                          title="SVM", id = "svm",
+                          tabPanel("Description","ICI NOUS ALLONS DECRIRE LA METHODE SVM"),
+                          tabPanel("Implementation","IMPLEMENTATION DU SVM")
+                  )
+                  
+                )),
         tabItem(tabName = "benchmark",
-                
-                tabBox (
-                  title="Comparison SVM with others Methods", id = "comparison",
-                  selectInput("methods","Choose Methods",c("Logistic regression","KNN","K-means","Boosting", "Random Forest", "Bagging") )
-                )
-                
-        ),
+                fluidPage(
+                  tabBox (
+                    title="Comparison SVM with others Methods", id = "comparison",
+                    selectInput("methods","Choose Methods",c("Logistic regression","KNN","K-means","Boosting", "Random Forest", "Bagging") )
+                  )
+                  
+                )),
         tabItem(tabName = "prediction",
-                selectInput("select_predict","Please Choose one way !",c("Load external data", "Enter data"))
-                
-                
-        ),
+                fluidPage(
+                  selectInput("select_predict","Please Choose one way !",c("Load external data", "Enter data"))
+                  
+                  
+                )),
         tabItem(tabName = "sample",
-                numericInput("select_train","Training proportion",min=0, max=1, value=0.7, step=0.01),
-                selectInput("select_sampling","Choose the method",c("Oversampling", "Undersampling", "SMOTE", "ADASYN", "Tomek-Link", "Tomek-Link + Undersampling","Condensed Nearest Neighbor" )),
-                uiOutput("sampling_out"),
-                dataTableOutput("out")
-       
-        )
+                fluidPage(
+                  numericInput("select_train","Training proportion",min=0, max=1, value=0.7, step=0.01),
+                  selectInput("select_sampling","Choose the method",c("Oversampling", "Undersampling", "SMOTE", "ADASYN", "Tomek-Link", "Tomek-Link + Undersampling","Condensed Nearest Neighbor" )),
+                  uiOutput("sampling_out"),
+                  dataTableOutput("out")
+                  
+                ))
+        
       )
-      
+    ),
+    rightsidebar = rightSidebar(),
+    footer = dashboardFooter(
+      left_text = h6(" © GROUP AKS OF MASTER ESA - Toute reproduction même partielle de la page est strictement interdite"),
+      right_text = h6("ORLEANS 2019")
     )
-    
+ 
   )
-  
 )
